337b59aecbf2801f2dc28de83bade396c22c0d9b
Michal <michalwit111@gmail.com>
2026-01-18 14:02:10 +0100

    Add SceneEntrance manually

--- tutorial-01/Scenes/GameScene/scene_entrance.gd
+++ tutorial-01/Scenes/GameScene/scene_entrance.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-@export var next_scene: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,14 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		print("asdf")
-		#get_tree().change_scene_to_file.call_deferred(next_scene)
-
-
-func _on_body_exited(body: Node2D) -> void:
-	print("Exited")
-	
