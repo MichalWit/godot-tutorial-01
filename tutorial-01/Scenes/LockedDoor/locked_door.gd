73b4b0447b28577e0a23e025e5a0ca1922152570
Michal Wit <michal.wit@paramount.com>
2026-01-21 15:45:49 +0100

    add pressed_buttons and buttons_needed vars

--- tutorial-01/Scenes/LockedDoor/locked_door.gd
+++ tutorial-01/Scenes/LockedDoor/locked_door.gd
@@ -1,13 +1,24 @@
 extends StaticBody2D
 
-func _ready() -> void:
-	pass
+@export var buttons_needed: int = 1
+var buttons_pressed: int = 0
 
 func _on_puzzle_button_pressed() -> void:
-	visible = false
-	$CollisionShape2D.set_deferred("disabled", true)
+	buttons_pressed += 1
+	
+	if buttons_pressed < buttons_needed:
+		visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
+		
+	print("buttons_pressed: " + str(buttons_pressed))
 
 
 func _on_puzzle_button_unpressed() -> void:
-	visible = true
-	$CollisionShape2D.set_deferred("disabled", false)
+	#if buttons_pressed > 0:
+	buttons_pressed -= 1
+	
+	if buttons_pressed >= buttons_needed:
+		visible = true
+		$CollisionShape2D.set_deferred("disabled", false)
+		
+	print("buttons_pressed: " + str(buttons_pressed))
