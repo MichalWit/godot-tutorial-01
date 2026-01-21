4668300436dfa69361b1e2fc34a9a2d6304ede38
Michal Wit <michal.wit@paramount.com>
2026-01-21 13:31:10 +0100

    enable collistion with door on button click fix

--- tutorial-01/Scenes/LockedDoor/locked_door.gd
+++ tutorial-01/Scenes/LockedDoor/locked_door.gd
@@ -2,16 +2,12 @@ extends StaticBody2D
 
 func _ready() -> void:
 	pass
-	#visible = true
 
 func _on_puzzle_button_pressed() -> void:
-	visible = !visible
-	if visible == false:
-		$CollisionShape2D.set_deferred("disabled", true)
-	else:
-		$CollisionShape2D.set_deferred("disabled", false)
+	visible = false
+	$CollisionShape2D.set_deferred("disabled", true)
 
 
 func _on_puzzle_button_unpressed() -> void:
-	#print("_on_puzzle_button_unpressed signal received")
-	pass
+	visible = true
+	$CollisionShape2D.set_deferred("disabled", false)
