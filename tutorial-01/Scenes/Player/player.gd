1f630103bd8958e608f46a1fd7d3fecd0459d711
Michal <michalwit111@gmail.com>
2026-01-23 21:14:04 +0100

    crete two private method in Player

--- tutorial-01/Scenes/Player/player.gd
+++ tutorial-01/Scenes/Player/player.gd
@@ -12,6 +12,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	__move_player()
+	__handle_collision()
+	move_and_slide()
+	
+	
+func __handle_collision() -> void:
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision != null:
+		var collider: Object = collision.get_collider()
+		if collider.is_in_group("pushable"):
+			var normal: Vector2 = collision.get_normal()
+			collider.apply_central_force(-normal * push_strength)
+			
+		if collider.is_in_group("wall"):
+			print("wall")
+
+
+func __move_player() -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_vector * move_speed
 	
@@ -25,15 +43,3 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("move_up")
 	else: #velocity == Vector2(0,0)
 		$AnimatedSprite2D.stop()
-		
-	var collision: KinematicCollision2D = get_last_slide_collision()
-	if collision != null:
-		var collider: Object = collision.get_collider()
-		if collider.is_in_group("pushable"):
-			var normal: Vector2 = collision.get_normal()
-			collider.apply_central_force(-normal * push_strength)
-			
-		if collider.is_in_group("wall"):
-			print("wall")
-	
-	move_and_slide()
