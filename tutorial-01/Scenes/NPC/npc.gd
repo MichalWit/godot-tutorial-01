b6ff4bb1c812de8ce48d494952c972f1048b866d
Michal <michalwit111@gmail.com>
2026-01-23 21:38:36 +0100

    Switch visibility of dialog on pressing X or space

--- tutorial-01/Scenes/NPC/npc.gd
+++ tutorial-01/Scenes/NPC/npc.gd
@@ -3,10 +3,11 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$CanvasLayer.visible = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
-		print("Hello there!")
+		$CanvasLayer.visible = !$CanvasLayer.visible
