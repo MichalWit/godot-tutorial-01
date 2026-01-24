6e4eefa5517345f7472012dc4177e5bdeddbf71d
Michal Wit <michal.wit@paramount.com>
2026-01-24 09:48:43 +0100

    Add multi step dialogue

--- tutorial-01/Scenes/NPC/npc.gd
+++ tutorial-01/Scenes/NPC/npc.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
 var can_interact: bool = false
+var dialogue_lines: Array[String] = ["Hello!", "Hi!", "Howdy!"]
+var dialogue_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +13,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
-		$CanvasLayer.visible = !$CanvasLayer.visible
-		if $CanvasLayer.visible:
+		if dialogue_index < dialogue_lines.size():
+			$CanvasLayer.visible = true
 			get_tree().paused = true
+			__forward_with_dialogue()
+			dialogue_index += 1
 		else:
+			$CanvasLayer.visible = false
 			get_tree().paused = false
+			dialogue_index = 0
+			
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func __forward_with_dialogue() -> void:
+	$CanvasLayer/Dialog.text = dialogue_lines[dialogue_index]
+		
