93416fb06248105869432adc9de7c2060ece17cf
Michal <michalwit111@gmail.com>
2026-01-25 13:25:04 +0100

    Close/Open dore on solved SwitchPuzzle

--- tutorial-01/Scenes/SwitchPuzzle/switch_puzzle.gd
+++ tutorial-01/Scenes/SwitchPuzzle/switch_puzzle.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+signal solved
+signal unresolved
+
 @export var combination: Array[bool] = [false, true, false, true]
 var score: int = 0
+var target_score: int = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +14,15 @@ func _ready() -> void:
 func inc() -> void:
 	score += 1
 	print("score: " + str(score))
+	emit()
 	
 func dec() -> void:
 	score -= 1
 	print("score: " + str(score))
-		
+	emit()
+	
+func emit() -> void:
+	if score == target_score:
+		solved.emit()
+	else:
+		unresolved.emit()
