b9a62e6156fc204ed21c0c564811ab06288c29dc
Michal <michalwit111@gmail.com>
2026-01-25 17:02:30 +0100

    Add TreasureChest - show scroll inside

---
+++ tutorial-01/Scenes/TreasureChest/treasure_chest.gd
@@ -0,0 +1,31 @@
+extends StaticBody2D
+
+var closed: bool = true
+var can_interact: bool = true
+
+func _ready() -> void:
+	$Scroll.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and can_interact:
+		if closed:
+			open()
+		else:
+			close()
+			
+func open() -> void:
+	$AnimatedSprite2D.play("open")
+	$Timer.start()
+	$Scroll.show()
+	closed = false
+			
+func close() -> void:
+	$AnimatedSprite2D.play("closed")
+	$Scroll.hide()
+	$Timer.stop()
+	closed = true
+
+
+func _on_timer_timeout() -> void:
+	$Scroll.hide()
