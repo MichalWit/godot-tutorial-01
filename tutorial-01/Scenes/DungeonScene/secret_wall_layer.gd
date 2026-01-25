1c8c281881ea6524e4861ca3971bb9d4b7cb8b63
Michal <michalwit111@gmail.com>
2026-01-25 12:14:51 +0100

    hide/show secret room

---
+++ tutorial-01/Scenes/DungeonScene/secret_wall_layer.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+func _on_switch_switch_off() -> void:
+	__enable_secret_wall()
+
+func _on_switch_switch_on() -> void:
+	__disable_secret_wall()
+	
+func __disable_secret_wall() -> void:
+	visible = false
+	collision_enabled = false
+
+func __enable_secret_wall() -> void:
+	visible = true
+	collision_enabled = true
